94b4c05e9a86e7a7f5c97f31bf4a6fc4404d9bb8
Md.Solaiman Chowdhury <solaiman6551@gmail.com>
2020-06-10 06:44:41 +0600

    Fibonacci solution added

---
+++ Fibonacci/fibonacci.py
@@ -0,0 +1,18 @@
+def fibonacci(n):
+    a = 0
+    b = 1
+    if n < 0:
+        print("Please enter a valid number")
+    elif n == 0:
+        return a
+    elif n == 1:
+        return b
+    else:
+        for i in range(2, n):
+            c = a + b
+            a = b
+            b = c
+        return b
+
+
+print(fibonacci(1000))
