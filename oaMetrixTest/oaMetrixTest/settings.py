57d71a7abd8707c1eee936a28338ddc735ee0ae2
Md.Solaiman Chowdhury <solaiman6551@gmail.com>
2020-06-10 16:07:57 +0600

    postgresql database connect

--- oaMetrixTest/oaMetrixTest/settings.py
+++ oaMetrixTest/oaMetrixTest/settings.py
@@ -84,23 +84,23 @@ REST_FRAMEWORK = {
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'oaMetrix',
-#         'USER': 'oaMetrix',
-#         'PASSWORD': 'oaMetrix',
-#         'HOST': '127.0.0.1',
-#         'PORT': '5432',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'oaMetrix',
+        'USER': 'oaMetrix',
+        'PASSWORD': 'oaMetrix',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
 
 
 # Password validation
